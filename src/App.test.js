0240e5994c5499b4e09f3c356a3a97983de5e50f
Matthijs G. Snijder <snijdermatthijs@gmail.com>
2021-05-03 13:30:47 +0200

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/react-in-jsx-scope */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App components mounts', () => {
+  beforeEach(() => {
+    render(<App />)
+  })
+  
+  it('renders text', async () => {
+    const text = screen.findAllByText('My App');
+
+    expect(text).toBeDefined;
+  })
+})
